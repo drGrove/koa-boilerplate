14eabf3502beda55ab397ae88ea3ca011d695ed5
Danny Grove <danny@drgrovellc.com>
2015-09-26 23:49:19 -0700

    test: Get tests working locally

--- gulp/test.js
+++ gulp/test.js
@@ -2,8 +2,10 @@ var gulp = require('gulp')
 var mocha = require('gulp-mocha-co')
 
 gulp.task('test', ['lint'], function() {
-  gulp.src([
-    './test/unit/**/**.spec.js'
-  ])
-    .pipe(mocha())
+  gulp
+    .src
+    ( [ './tests/unit/**/**.spec.js'
+      ]
+    )
+    .pipe(mocha({ reporter: 'nyan'}))
 })
--- tests/unit/lib/db.spec.js
+++ tests/unit/lib/db.spec.js
@@ -1,26 +1,29 @@
 'use strict'
+var expect = require('expect')
 var db = require(process.env.PROJECT_ROOT + '/lib/db.js')
 
-describe('DB Connection Tests:', function() {
-  it('Should show this', function() {
+describe('DB Connection Tests:', function () {
+  it('Should show this', function () {
     expect(false).toBe(false)
   })
 
-  it('Should be an object', function() {
+  it('Should be an object', function () {
     expect(typeof db).toBe('object');
   })
 
   if (db.sequelize) {
-    var sequelize = db.sequelize
-    it ('Should have a connection to MySQL', function *() {
-      let stmt = 'SHOW TABLES'
-      let tables = yield sequelize
-        .query
-        ( stmt
-        , { type: sequelize.QueryTypes.SHOWTABLES
-          }
-        )
-      expect(Array.isArray(tables)).toBe(true)
+    it('Should be able to show tables', function *() {
+      var sequelize = db.sequelize
+      it ('Should have a connection to MySQL', function *() {
+        let stmt = 'SHOW TABLES'
+        let tables = yield sequelize
+          .query
+          ( stmt
+          , { type: sequelize.QueryTypes.SHOWTABLES
+            }
+          )
+        expect(Array.isArray(tables)).toBe(true)
+      })
     })
   }
 })
